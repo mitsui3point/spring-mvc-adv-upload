52a3c9f468b0492cc0e248902d3c9ce4c457bfcc
mitsui3point <rladbsgh0331@gmail.com>
2023-02-27 23:20:53 +0900

    feat($file, $test.file): update -update: fileName encode method add

--- src/main/java/hello/upload/controller/ItemController.java
+++ src/main/java/hello/upload/controller/ItemController.java
@@ -6,6 +6,7 @@ import hello.upload.dto.ItemResult;
 import hello.upload.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.buf.UriUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
@@ -15,9 +16,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Controller
@@ -54,11 +57,12 @@ public class ItemController {
                 .getAttachFile();
         String storeFileName = attachFile.getStoreFileName();
         String uploadFileName = attachFile.getUploadFileName();
+        String encodedUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
 
         Resource resource = itemService.downloadFile(storeFileName);
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + uploadFileName + "\"")
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + encodedUploadFileName + "\"")
                 .header(HttpHeaders.CONTENT_TYPE, "application/octet-stream")
                 .body(resource);
     }
